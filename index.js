ca648579176761d244e130a44368ff1cb2c087ef
Charlie McConnell <charlie@getpebble.com>
2015-03-18 16:38:53 -0400

    Reformatting, various fixes for dynamic log token handling

--- index.js
+++ index.js
@@ -2,68 +2,69 @@ var request = require('request');
 require('array.prototype.find');
 
 var sources = {
-	token: 'token',
-	udp: 'syslog'
+  token: 'token',
+  udp: 'syslog'
 };
 
 var apiUri = 'https://api.logentries.com';
 
 function api(data, cb) {
-	var options = {
-		uri: apiUri,
-		method: 'POST',
-		form: data
-	};
-	request(options, function(err, res, body) {
-		if (err) return cb(err);
-		var result = JSON.parse(body);
-		if (result.response === 'error') return cb(new Error(result.reason));
-		return cb(null, result);
-	});
+  var options = {
+    uri: apiUri,
+    method: 'POST',
+    form: data
+  };
+  request(options, function(err, res, body) {
+    if (err) return cb(err);
+    var result = JSON.parse(body);
+    if (result.response === 'error') return cb(new Error(result.reason));
+    return cb(null, result);
+  });
 }
 
 module.exports = function(options) {
-	return {
-		getHosts: function(cb) {
-			api({
-				request: 'get_user',
-				load_hosts: 1,
-				user_key: options.accountKey
-			}, cb);
-		},
+  return {
+    getHosts: function(cb) {
+      api({
+        request: 'get_user',
+        load_hosts: 1,
+        load_logs: 1,
+        user_key: options.accountKey
+      }, cb);
+    },
 
-		getHost: function(hostname, cb) {
-			this.getHosts(function(err, result) {
-				if (err) return cb(err);
-				var host = result.hosts.find(function(host) {
-					return host.name === hostname;
-				});
-				return cb(null, host);
-			});
-		},
+    getHost: function(hostname, cb) {
+      this.getHosts(function(err, result) {
+        if (err) return cb(err);
+        var host = result.hosts.find(function(host) {
+          return host.name === hostname;
+        });
+        return cb(null, host);
+      });
+    },
 
-		registerHost: function(host, cb) {
-			api({
-				request: 'register',
-				user_key: options.accountKey,
-				name: host,
-				hostname: 'nolocation'
-			}, cb);
-		},
+    registerHost: function(host, cb) {
+      api({
+        request: 'register',
+        user_key: options.accountKey,
+        name: host,
+        hostname: 'AWS us-west-2'
+      }, cb);
+    },
 
-		createLog: function(name, type, hostKey, cb) {
-			if (!sources[type]) return cb(new Error('Invalid log type.'));
+    createLog: function(name, type, hostKey, cb) {
+      if (!sources[type]) return cb(new Error('Invalid log type.'));
 
-			api({
-				request: 'new_log',
-				user_key: options.accountKey,
-				host_key: hostKey,
-				source: sources.udp,
-				name: name,
-				retention: '-1'
-			}, cb);
-		}
-	};
+      api({
+        request: 'new_log',
+        user_key: options.accountKey,
+        host_key: hostKey,
+        source: sources.token,
+        name: name,
+        retention: '-1'
+      }, cb);
+    }
+  };
 };
 
 
